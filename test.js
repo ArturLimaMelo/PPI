208fda30abb9ad5cf8d60836333039856c6af72f
Artur Lima Melo <20221074010012@ifrn.local>
2025-04-25 13:43:40 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,147 @@
+/* 
+ Programação para Internet 2025
+ 4INF2V
+*/
+
+// Definição de variáveis e operadores
+// let a = 5;
+// a = 'Eu';
+// console.log(a);
+
+// const b = 3.14;
+// console.log(b);
+
+// Operadores
+// Aritméticos: + - * / % **
+// Incremento: ++, --
+// Relacionais: === !==
+
+// let x = 5 + 5;
+// let y = '5' + 5;
+// let z = 'Hello' + 5;
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// console.log(typeof x === 'number');
+
+// if(x === 1) {
+//     console.log('1');
+// } else if(x === 2){
+//     console.log('2');
+// } else {
+//     console.log('Diferente');
+// }
+
+// for(let i=0; i<10; i++){
+//     console.log(i);
+// }
+
+// Arrays ==================================================================
+
+// const a = [1, 'Gui', true];
+// console.log(a);
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const fruits2 = ["Kiwi", "Lemon", "Melon", "Blueberry"];
+
+// console.log(fruits.length);
+// console.log(fruits[0]);
+
+// Insert in the end
+// fruits.push('Watermelon');
+// console.log(fruits);
+
+// Insert in the beginning
+// fruits.unshift('Tangerine');
+// console.log(fruits);
+
+// Remove first element
+// fruits.shift();
+// console.log(fruits);
+
+// Remove last element
+// fruits.pop();
+// console.log(fruits);
+
+// console.log(fruits.concat(fruits2));
+
+// console.log(fruits);
+// fruits.splice(2, 1);
+// console.log(fruits);
+
+// fruits.splice(2, 0, 'Tangerine', 'Watermelon');
+// console.log(fruits);
+
+// console.log(fruits2);
+// fruits.splice(3, 0, ...fruits2);
+// console.log(fruits);
+
+// let fruits3 = ['Tangerine', ...fruits];
+// fruits3 = [...fruits3, 'Watermelon'];
+// console.log(fruits3);
+
+const numbers = [9, 2, 8, 77, 10];
+/*
+console.log(numbers);
+
+// numbers.sort();
+console.log(numbers.toSorted((a, b) => a - b)); // C
+console.log(numbers.toSorted((a, b) => b - a)); // D
+
+const numbers_ord = numbers.toSorted((a, b) => a - b);
+console.log(numbers_ord[0]); //min
+console.log(numbers_ord[numbers.length - 1]); //max
+
+// numbers.reverse();
+// console.log(numbers.toReversed());
+
+// let n = [];
+// for(let i=0; i<numbers.length; i++){
+//     n.push(numbers[i]*2);
+// }
+// console.log(n);
+
+// function myFunction(value, index, array){
+//     return value * 2;
+// }
+
+// console.log(numbers.map(myFunction));
+
+// Arrow functions
+// console.log(numbers.map((number) => number * 2));
+
+console.log(fruits);
+console.log(fruits.find((fruit) => fruit[0].toLowerCase() === "b"));
+
+console.log(fruits.findIndex((fruit) => fruit[0].toLowerCase() === "b"));
+
+const fruits4 = [...fruits, ...fruits2];
+console.log(fruits4);
+
+// Exibir todas as frutas que comecem com "letra"
+let letra = "m";
+let out = [];
+fruits4.map((fruit) => {
+  if (fruit[0].toLowerCase() === letra) {
+    out.push(fruit);
+  }
+});
+console.log(out);
+
+console.log(fruits4.filter((fruit) => fruit[0].toLowerCase() === letra));
+*/
+
+
+
+
+
+let soma = 0;
+//numbers.map((number) => soma += number);
+//console.log(soma);
+
+//console.log(numbers.reduce(soma, number) == soma + number);
+
+// Objects
+
