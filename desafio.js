df2c02fd9782ee28e03778031949224f22f01471
ArturLimaMelo <151844637+ArturLimaMelo@users.noreply.github.com>
2025-05-09 13:28:06 -0300

    Filter

--- desafio.js
+++ desafio.js
@@ -21,18 +21,18 @@ class Carrinho {
     }
   
     ordenarPorNome() {
-        return this.itens.toSorted((a, b) => a.name.localeCompare(b.nome));
+        return this.itens.toSorted();
     }
   }
   
 const carrinho = new Carrinho([
-    {name: "Macbook Pro M4", storage: 512,ram:  16,price:  1599.90, quantity: 2, category: "Macbook", total: 3199.80},
-    {name: "Macbook Air M4",storage:  256,ram:  16,price:  1099.90, quantity: 5, category: "Macbook", total: 5499.50},
     {name: "Apple Watch Series 10 46mm",storage:  64,ram:  16,price:  429.90, quantity: 3, category: "Watch", total: 1289.70},
     {name: "iphone 16", storage: 128, ram: 16, price: 899.90, quantity: 10, category: "iPhone", total: 8990.90},
     {name: "iPhone 16 Pro Max", storage: 1024, ram: 16, price: 1199.90, quantity: 1, category: "iPhone", total: 1199.90},
     {name: "iMac M4", storage: 512,ram: 24, price: 1799.90, quantity: 1, category: "iMac", total: 1799.90},
-    {name: "iPad Pro 13in", storage: 2048, ram:8, price: 2199.90, quantity: 1, category: "iPad", total: 2199.90}
+    {name: "iPad Pro 13in", storage: 2048, ram:8, price: 2199.90, quantity: 1, category: "iPad", total: 2199.90},
+    {name: "Macbook Pro M4", storage: 512,ram:  16,price:  1599.90, quantity: 2, category: "Macbook", total: 3199.80},
+    {name: "Macbook Air M4",storage:  256,ram:  16,price:  1099.90, quantity: 5, category: "Macbook", total: 5499.50}
 ]);
 
 console.log("Valor total do carrinho: " + carrinho.calcularTotal());
